113844de3b324ec39f6f6ee44b865cf3992446b7
Fabricio Godoy <skarllot@gmail.com>
2017-08-09 01:37:59 -0300

    Create variant alternatives for Action and Func

--- src/NETStandard.WindowsCE/Action.cs
+++ src/NETStandard.WindowsCE/Action.cs
@@ -6,6 +6,18 @@ namespace System
 namespace Mock.System
 #endif
 {
+    // Variant alternatives
+    public delegate void Action2<in T1>(T1 arg1);
+    public delegate void Action2<in T1, in T2>(T1 arg1, T2 arg2);
+    public delegate void Action2<in T1, in T2, in T3>(T1 arg1, T2 arg2, T3 arg3);
+    public delegate void Action2<in T1, in T2, in T3, in T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+    public delegate TResult Func2<out TResult>();
+    public delegate TResult Func2<in T, out TResult>(T arg);
+    public delegate TResult Func2<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
+    public delegate TResult Func2<in T1, in T2, in T3, out TResult>(T1 arg1, T2 arg2, T3 arg3);
+    public delegate TResult Func2<in T1, in T2, in T3, in T4, out TResult>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+
+    // Additional
     public delegate void Action<in T1, in T2, in T3, in T4, in T5>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
     public delegate void Action<in T1, in T2, in T3, in T4, in T5, in T6>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
     public delegate void Action<in T1, in T2, in T3, in T4, in T5, in T6, in T7>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
