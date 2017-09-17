f5e517e9c052401896f110e8eff5e5192d1270b8
Fabricio Godoy <skarllot@gmail.com>
2017-09-17 01:40:00 -0300

    Fix ArgumentNullException2 inheritance

--- src/NETStandard.WindowsCE/ArgumentNullException2.cs
+++ src/NETStandard.WindowsCE/ArgumentNullException2.cs
@@ -3,6 +3,12 @@ using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 
 #if NET35_CF
+using System.Runtime.ExceptionServices;
+#else
+using Mock.System.Runtime.ExceptionServices;
+#endif
+
+#if NET35_CF
 namespace System
 #else
 namespace Mock.System
@@ -14,32 +20,49 @@ namespace Mock.System
     /// </summary>
     [ComVisible(true)]
     [Serializable]
-    public class ArgumentNullException2 : ArgumentException2
+    public class ArgumentNullException2 : ArgumentNullException, ISerializable
     {
+        private readonly string _paramName;
+
         /// <summary>
         /// Creates a new ArgumentNullException with its message
         /// string set to a default message explaining an argument was null.
         /// </summary>
         public ArgumentNullException2()
-            : base(ExtractMessage())
+            : base()
         { }
 
         public ArgumentNullException2(string paramName)
-            : base(ExtractMessage(), paramName)
-        { }
+            : base(paramName)
+        {
+            _paramName = paramName;
+        }
 
         public ArgumentNullException2(string message, Exception innerException)
-            : base(message, innerException)
-        { }
+            : base(null, message)
+        {
+            this.SetInnerException(innerException);
+        }
 
         public ArgumentNullException2(string paramName, string message)
-            : base(message, paramName)
-        { }
+            : base(paramName, message)
+        {
+            _paramName = paramName;
+        }
 
         protected ArgumentNullException2(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
+        {
+            ExceptionSerializer.SetObjectData(this, info, context);
+            _paramName = info.GetString("ParamName");
+        }
 
-        private static string ExtractMessage()
-            => new ArgumentNullException().Message;
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new ArgumentNullException(nameof(info));
+
+            ExceptionSerializer.GetObjectData(this, info, context);
+            info.AddValue("ParamName", _paramName, typeof(string));
+        }
     }
 }
