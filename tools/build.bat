0851e6abbd2b5448e1d4ec706eedd8c9e601b493
Fabrício Godoy <skarllot@gmail.com>
2018-03-04 21:22:11 -0300

    Support 'D:' install location

--- tools/build.bat
+++ tools/build.bat
@@ -27,6 +27,9 @@ if not exist %msbuild% (
     set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 )
 if not exist %msbuild% (
+    set msbuild="D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+)
+if not exist %msbuild% (
 	echo Error trying to find MSBuild executable
 	EXIT /B 1
 )
