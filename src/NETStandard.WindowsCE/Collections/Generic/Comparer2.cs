3e0532f77d12ebebf1bebaba4fcb1f6a75699a29
Fabrício Godoy <skarllot@gmail.com>
2018-12-06 23:42:43 -0200

    Extend System.Collections.Generic.Comparer (Fix #34)

---
+++ src/NETStandard.WindowsCE/Collections/Generic/Comparer2.cs
@@ -0,0 +1,28 @@
+﻿namespace System.Collections.Generic
+{
+    public static class Comparer2<T>
+    {
+        public static Comparer<T> Create(Comparison<T> comparison)
+        {
+            if (comparison == null)
+                throw new ArgumentNullException(nameof(comparison));
+
+            return new ComparisonComparer<T>(comparison);
+        }
+    }
+
+    public sealed class ComparisonComparer<T> : Comparer<T>
+    {
+        private readonly Comparison<T> _comparison;
+
+        public ComparisonComparer(Comparison<T> comparison)
+        {
+            _comparison = comparison;
+        }
+
+        public override int Compare(T x, T y)
+        {
+            return _comparison(x, y);
+        }
+    }
+}
