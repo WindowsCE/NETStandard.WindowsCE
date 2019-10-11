4e97e0a0d26c36f5244878c3f836216ab3f86f79
Fabricio Godoy <skarllot@gmail.com>
2019-10-11 17:54:28 -0300

    Support non-indexed GetValue for PropertyInfo (Close #42)

---
+++ src/NETStandard.WindowsCE/Reflection/PropertyInfoExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+#nullable enable
+
+namespace System.Reflection
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class PropertyInfoExtensions
+    {
+        public static object? GetValue(this PropertyInfo propertyInfo, object? obj)
+        {
+            return propertyInfo.GetValue(obj, null);
+        }
+    }
+}
