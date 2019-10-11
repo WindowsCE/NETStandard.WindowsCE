7c32d9c77e3de90da7577176090488e8846a1760
Fabricio Godoy <skarllot@gmail.com>
2019-10-11 17:41:45 -0300

    Create LocalizableAttribute (Close #44)

---
+++ src/NETStandard.WindowsCE/Attribute2.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+namespace System
+{
+    public class Attribute2
+        : Attribute
+    {
+        public virtual object TypeId => GetType();
+
+        public virtual bool IsDefaultAttribute() => false;
+    }
+}
