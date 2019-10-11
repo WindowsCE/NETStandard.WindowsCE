4196a205a517950fa53880ccce5217bc3e64338e
Fabricio Godoy <skarllot@gmail.com>
2019-10-11 18:01:31 -0300

    Create fake ExecutionContext

---
+++ src/NETStandard.WindowsCE/Threading/ExecutionContext.cs
@@ -0,0 +1,16 @@
+﻿namespace System.Threading
+{
+    public delegate void ContextCallback(object state);
+
+    public class ExecutionContext
+    {
+        private static readonly ExecutionContext Default = new ExecutionContext();
+
+        public static ExecutionContext Capture() => Default;
+
+        public static void Run(ExecutionContext executionContext, ContextCallback callback, object state)
+        {
+            callback(state);
+        }
+    }
+}
