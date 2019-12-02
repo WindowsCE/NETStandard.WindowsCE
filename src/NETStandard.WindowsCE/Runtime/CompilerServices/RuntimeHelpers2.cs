9a345f5205091696745079b69fcebae805840b76
Fabrício Godoy <skarllot@gmail.com>
2019-12-02 15:21:13 -0300

    Support GetSubArray

--- src/NETStandard.WindowsCE/Runtime/CompilerServices/RuntimeHelpers2.cs
+++ src/NETStandard.WindowsCE/Runtime/CompilerServices/RuntimeHelpers2.cs
@@ -33,10 +33,49 @@ namespace System.Runtime.CompilerServices
         }
 
         public static object GetObjectValue(object obj)
-            => RuntimeHelpers.GetObjectValue(obj);
+        {
+            return RuntimeHelpers.GetObjectValue(obj);
+        }
+
+#nullable enable
+        /// <summary>Slices the specified array using the specified range.</summary>
+        // Ref: https://gist.github.com/bgrainger/fb2c18659c2cdfce494c82a8c4803360
+        public static T[] GetSubArray<T>(T[] array, Range range)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            (int offset, int length) = range.GetOffsetAndLength(array.Length);
+
+            if (default(T)! != null || typeof(T[]) == array.GetType()) // TODO-NULLABLE: default(T) == null warning (https://github.com/dotnet/roslyn/issues/34757)
+            {
+                // We know the type of the array to be exactly T[].
+
+                if (length == 0)
+                {
+                    return Array2.Empty<T>();
+                }
+
+                var dest = new T[length];
+                Array.Copy(array, offset, dest, 0, length);
+                return dest;
+            }
+            else
+            {
+                // The array is actually a U[] where U:T.
+                T[] dest = (T[])Array.CreateInstance(array.GetType().GetElementType()!, length);
+                Array.Copy(array, offset, dest, 0, length);
+                return dest;
+            }
+        }
+#nullable disable
 
         public static void InitializeArray(Array array, RuntimeFieldHandle fldHandle)
-            => RuntimeHelpers.InitializeArray(array, fldHandle);
+        {
+            RuntimeHelpers.InitializeArray(array, fldHandle);
+        }
 
         // TODO: Improve for real usage
         public static void RunClassConstructor(RuntimeTypeHandle typeHandle)
